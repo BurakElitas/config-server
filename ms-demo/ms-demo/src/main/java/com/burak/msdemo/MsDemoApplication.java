e99a355bec1a58ac578838e0c3edc3997ff83515
BurakElitas <48959177+BurakElitas@users.noreply.github.com>
2020-01-31 01:11:04 +0300

    first

---
+++ ms-demo/ms-demo/src/main/java/com/burak/msdemo/MsDemoApplication.java
@@ -0,0 +1,15 @@
+package com.burak.msdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDemoApplication.class, args);
+	}
+
+}
---
+++ ms-demo/ms-demo/src/main/java/com/burak/msdemo/controller/DemoController.java
@@ -0,0 +1,27 @@
+package com.burak.msdemo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+@RefreshScope
+public class DemoController {
+
+    @Value("${message}")
+    private String message;
+
+    @GetMapping(value ="")
+    public String getNames(){
+        List<String> names=new ArrayList<>();
+        names.add("Burak");
+        names.add("Gülşah");
+        return message;
+    }
+}
